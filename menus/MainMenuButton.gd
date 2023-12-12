5a6228cd50174438860598c3a949545199d6ab4f
patch <98074289+patchrick89@users.noreply.github.com>
2023-12-11 21:51:39 -0500

    I forget but I know I am struggling with Settings Menu button

--- menus/MainMenuButton.gd
+++ menus/MainMenuButton.gd
@@ -4,4 +4,5 @@ func _ready():
 	connect("pressed", _on_MainMenu_button_pressed)
 
 func _on_MainMenu_button_pressed():
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://menus/TitleScreen.tscn")
