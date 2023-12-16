ee9368d0ed7a26fe2371fb2462b188c94c2ab8c2
dinger086 <dinger900@gmail.com>
2023-12-15 22:18:54 -0500

    Add new enemy boss sprites and update title screen functionality

--- menus/TitleScreen.gd
+++ menus/TitleScreen.gd
@@ -21,7 +21,6 @@ func _on_start_button_pressed():
 
 func _on_settings_button_pressed():
 	get_tree().change_scene_to_file("res://menus/Settings.tscn")
-	self.visible = true
 
 func _on_quit_button_pressed():
 	get_tree().quit()
